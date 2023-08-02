c055d212468f292d43193e038178a08db9f285c0
aurelienrichard <a.richard.dev@gmail.com>
2023-08-02 11:35:31 +0200

    first commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,35 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-  },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
-};
+	env: {
+		node: true,
+		browser: true
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	extends: [
+		'airbnb-base',
+		'airbnb-typescript/base',
+		'plugin:@typescript-eslint/recommended',
+		'turbo',
+		'prettier'
+	],
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'import/prefer-default-export': 'off',
+		'import/no-default-export': 'error',
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': 'error',
+		'lines-between-class-members': 'off',
+		'@typescript-eslint/lines-between-class-members': [
+			'error',
+			'always',
+			{ exceptAfterSingleLine: true }
+		],
+		'@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
+		'no-param-reassign': ['error', { props: false }],
+		eqeqeq: ['error', 'smart']
+	}
+}
