1cec148ae3902df9b0420825c15c5b21e276d1da
aurelienrichard <a.richard.dev@gmail.com>
2023-08-09 20:14:41 +0200

    ematchi sveltekit app setup

---
+++ apps/ematchi/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	let name = ''
+	const arr = []
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<input bind:value={name} />
+<p>hello {name}</p>
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,14 +1,4 @@
 module.exports = {
-	env: {
-		node: true,
-		browser: true
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module'
-		// extraFileExtensions: ['.svelte']
-	},
 	extends: [
 		'airbnb-base',
 		'airbnb-typescript/base',
@@ -17,7 +7,19 @@ module.exports = {
 		'turbo',
 		'prettier'
 	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		extraFileExtensions: ['.svelte']
+	},
 	plugins: ['@typescript-eslint'],
+	env: {
+		node: true,
+		browser: true
+	},
 	ignorePatterns: ['node_modules', '*.cjs'],
-	rules: {}
+	rules: {
+		'import/no-extraneous-dependencies': ['error', { devDependencies: true }]
+	}
 }
