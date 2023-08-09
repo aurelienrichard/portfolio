167831a7f58c7d04dad1d9b4fcefec8783301e27
aurelienrichard <a.richard.dev@gmail.com>
2023-08-09 16:49:16 +0200

    ematchi-game sveltekit app first commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -7,30 +7,17 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module'
+		// extraFileExtensions: ['.svelte']
 	},
 	extends: [
 		'airbnb-base',
 		'airbnb-typescript/base',
 		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
 		'turbo',
 		'prettier'
 	],
 	plugins: ['@typescript-eslint'],
-	ignorePatterns: ['node_modules'],
-	rules: {
-		'import/prefer-default-export': 'off',
-		'import/no-default-export': 'error',
-		'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': 'error',
-		'lines-between-class-members': 'off',
-		'@typescript-eslint/lines-between-class-members': [
-			'error',
-			'always',
-			{ exceptAfterSingleLine: true }
-		],
-		'@typescript-eslint/no-explicit-any': ['error', { ignoreRestArgs: true }],
-		'no-param-reassign': ['error', { props: false }],
-		eqeqeq: ['error', 'smart']
-	}
+	ignorePatterns: ['node_modules', '*.cjs'],
+	rules: {}
 }
