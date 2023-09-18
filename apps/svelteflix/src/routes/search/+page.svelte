4ca37193540706744dbd58e529a5cd442b209353
aurelienrichard <a.richard.dev@gmail.com>
2023-09-18 15:00:09 +0200

    update search page with submit button and better accessibility

--- apps/svelteflix/src/routes/search/+page.svelte
+++ apps/svelteflix/src/routes/search/+page.svelte
@@ -7,19 +7,28 @@
 		query: string
 		movies: MovieListResult[]
 	}
+
+	let { query } = data
+	$: disabled = !query
 </script>
 
 <div class="pt-10">
 	<h1 class="text-4xl font-bold sm:text-5xl lg:text-6xl">Search</h1>
 
-	<form class="py-4 sm:py-6 lg:py-8">
+	<form class="flex py-4 sm:py-6 lg:py-8">
 		<input
 			type="text"
-			class="focus-visible:outline-accent w-full rounded-md bg-[rgba(255,255,255,0.1)] p-2 text-xl focus-visible:outline focus-visible:outline-2 sm:text-2xl lg:text-3xl"
+			class="focus-visible:outline-accent w-full rounded-s-md bg-[rgba(255,255,255,0.1)] p-2 text-xl focus-visible:outline focus-visible:outline-2 sm:text-2xl lg:text-3xl"
 			name="query"
-			value={data.query}
+			bind:value={query}
 			required
 		/>
+		<button
+			class:bg-zinc-700={disabled}
+			class="bg-accent w-40 rounded-e-md text-base transition-colors duration-200 sm:w-44 sm:text-lg lg:w-48 lg:text-xl"
+			type="submit"
+			{disabled}>Find movies</button
+		>
 	</form>
 
 	{#if data.query}
