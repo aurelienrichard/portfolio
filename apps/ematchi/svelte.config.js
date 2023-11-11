d50e7fd249ca131d52b4d97200370685ec6bffcc
aurelienrichard <a.richard.dev@gmail.com>
2023-11-11 23:10:55 +0100

    update svelte config to exclude static assets

--- apps/ematchi/svelte.config.js
+++ apps/ematchi/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			routes: {
-				exclude: ['/twemoji/*']
+				exclude: ['/_app/*', '/twemoji/*', '/favicon.png', '/pause-circle.svg']
 			}
 		})
 	}
