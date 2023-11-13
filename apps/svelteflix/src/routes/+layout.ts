447a20a95ee0f9f425107cd4021dfd0e5e297e1e
aurelienrichard <a.richard.dev@gmail.com>
2023-11-13 21:13:48 +0100

    update environment variables naming for CI

--- apps/svelteflix/src/routes/+layout.ts
+++ apps/svelteflix/src/routes/+layout.ts
@@ -1,13 +1,16 @@
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit'
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+import {
+	PUBLIC_SVELTEFLIX_SUPABASE_ANON_KEY,
+	PUBLIC_SVELTEFLIX_SUPABASE_URL
+} from '$env/static/public'
 import type { LayoutLoad } from './$types'
 
 export const load: LayoutLoad = async ({ fetch, data, depends, url: { pathname } }) => {
 	depends('supabase:auth')
 
 	const supabase = createSupabaseLoadClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: PUBLIC_SVELTEFLIX_SUPABASE_URL,
+		supabaseKey: PUBLIC_SVELTEFLIX_SUPABASE_ANON_KEY,
 		event: { fetch },
 		serverSession: data.session
 	})
