08aae776d6e0ce11b9ab66b6d336b1e704314a63
aurelienrichard <a.richard.dev@gmail.com>
2024-01-15 17:46:06 +0100

    replace modal with new route for creating new room

--- apps/privelte/src/routes/+page.svelte
+++ apps/privelte/src/routes/+page.svelte
@@ -1,8 +1,16 @@
-<div class="text-center">
+<div class="mb-12 text-center">
 	<h1
-		class="dark:from-gradient-1-dark h1 dark:to-gradient-2-dark from-gradient-1-light to-gradient-2-light leading mb-4 bg-gradient-to-br box-decoration-clone bg-clip-text capitalize leading-snug text-transparent"
+		class="dark:from-gradient-1-dark h1 dark:to-gradient-2-dark from-gradient-1-light to-gradient-2-light leading mb-6 bg-gradient-to-br box-decoration-clone bg-clip-text capitalize leading-snug text-transparent underline md:mb-8"
 	>
-		chat privately and anonymously.
+		chat <span
+			class="relative before:absolute before:-inset-1 before:block before:bg-gradient-to-br before:bg-[length:100%_2px] before:bg-bottom before:bg-no-repeat md:before:-inset-2"
+			>privately</span
+		>
+		and
+		<span
+			class="relative before:absolute before:-inset-1 before:block before:bg-gradient-to-br before:bg-[length:100%_2px] before:bg-bottom before:bg-no-repeat md:before:-inset-2"
+			>anonymously</span
+		>.
 	</h1>
 	<p class="md:text-xl">
 		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum voluptatem quam deserunt
