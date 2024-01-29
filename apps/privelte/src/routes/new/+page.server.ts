9ab634a35150df7a473bd8e5ffe9ffc8167c7b21
aurelienrichard <a.richard.dev@gmail.com>
2024-01-29 14:27:36 +0100

    update server action on create new room form submission with error boundary

--- apps/privelte/src/routes/new/+page.server.ts
+++ apps/privelte/src/routes/new/+page.server.ts
@@ -1,33 +1,49 @@
 import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator'
 import { z } from 'zod'
+import { error, redirect } from '@sveltejs/kit'
 import type { Actions, PageServerLoad } from './$types'
 
+let currentId: string
+
 const schema = z.object({
-	url: z.string(),
+	id: z.string(),
 	slots: z.coerce.number().min(2).max(10)
 })
 
 export const load = (() => {
-	const url = uniqueNamesGenerator({
+	const id = uniqueNamesGenerator({
 		dictionaries: [adjectives, colors, animals],
 		separator: '-',
 		style: 'lowerCase',
 		length: 3
 	})
 
+	currentId = id
+
 	return {
-		url
+		id
 	}
 }) satisfies PageServerLoad
 
 export const actions: Actions = {
 	default: async ({ request }) => {
 		const data = await request.formData()
-		const { url, slots } = schema.parse({
-			url: data.get('url'),
+		const { id, slots } = schema.parse({
+			id: data.get('id'),
 			slots: data.get('slots')
 		})
 
-		console.log({ url, slots })
+		console.log({ id, slots })
+
+		if (id !== currentId) {
+			error(400, { message: 'Invalid ID' })
+		}
+
+		// check if id already exists
+		// if it does, error 500
+
+		// create room with id and slots
+
+		throw redirect(303, `/room/${id}`)
 	}
 }
