f9d466c7fb23ef39ea1b00ac5621e5b1713e2e54
aurelienrichard <a.richard.dev@gmail.com>
2024-02-01 19:39:24 +0100

    database connection & basic chat room layout

---
+++ apps/privelte/src/lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_PRIVELTE_SUPABASE_URL, PUBLIC_PRIVELTE_SUPABASE_ANON_KEY } from '$env/static/public'
+
+export const supabase = createClient(
+	PUBLIC_PRIVELTE_SUPABASE_URL,
+	PUBLIC_PRIVELTE_SUPABASE_ANON_KEY
+)
